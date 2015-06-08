874d19a0202e7afcda7437a9da2ea03c451ab8eb
niosega <niosega>
2015-06-08 17:13:16 +0200

    Ajout des tests

--- Test/test.js
+++ Test/test.js
@@ -1,13 +1,23 @@
-function sayHello(){
-	
-	var openFile = function(event) {
-		var input = event.target;
-		var reader = new FileReader();
-		reader.onload = function(){
-			  var text = reader.result;
-			  console.log(text);
-		};
-		reader.readAsText(input.files[0]);
-	};
+//~ Affiche dans la console et dans la textarea la chaine str
+function affiche(str){
+	var zoneTexte = document.getElementById("textDiv");
+	zoneTexte.textContent=zoneTexte.textContent+"\n"+str+"\n";
+	console.log(str);
+}
+
+//~ Compte le nombre d'occurence du caractere char dans la chaine str
+function count(str,char){
+	var temp = str;
+	var compteur =0;
+	for(var i =0;i<str.length;i++){
+		if(str.charAt(i)==char){
+			compteur++;
+		}
+	}
+	return compteur;
+}
+
+			
+function parse(vcd){
 	
 }
