2b894285e9d576ebeddc1fcf5f6ac92da1e736aa
niosega <niosega>
2015-06-10 11:05:38 +0200

    Amelioration du parse

--- Test/test.js
+++ Test/test.js
@@ -1,18 +1,32 @@
 //~ Object qui représente une variable 
-function Variable(type,length,id,name,value) {
+function Variable(type,length,id,scope,name) {
 	this.type=type;
 	this.length=length;
 	this.id=id;
+	this.scope=scope;
 	this.name=name;
-	this.value=value;
+	this.lastValue;
+	this.value = new Map();
 	
 	this.toString = function(){
-		return "type : "+type+" length : "+length+" id : "+id+" name : "+name+" value : "+value;
+		return "type : "+type+" length : "+length+" id : "+id+" scope : "+scope +" name : "+name;
 	}
 	
 	this.changeValue = function(value){
 		this.value=value;
 	}
+	
+	this.afficheTemps = function(){
+		this.value.forEach(function(value,key){
+			affiche("Temps "+name+" : "+key+" | "+value);
+		},this.value)
+	}
+	
+	this.afficheTempsA = function(t){
+		var x = this.value.get(t);
+		return x;
+	}
+	
 }
 
 //~ Affiche dans la console et dans la textarea la chaine str
@@ -48,13 +62,31 @@ function count(str,char){
 }
 
 //~ Change la valeur associé la clé key dans la map 
-function changeMapValue(map,key,newValue){
+function changeMapValue(map,key,newTime,newValue){
 	var ancienneVariable = map.get(key);
+	var ancienneValeur = ancienneVariable.value;
 	map.delete(key);
-	var nouvelleVariable = new Variable(ancienneVariable.type,ancienneVariable.length,ancienneVariable.id,ancienneVariable.name,newValue);
+	var nouvelleVariable = new Variable(ancienneVariable.type,ancienneVariable.length,ancienneVariable.id,ancienneVariable.scope,ancienneVariable.name);
+	nouvelleVariable.value = ancienneValeur;
+	nouvelleVariable.lastValue = newValue;
+	if(nouvelleVariable.value.has(newTime)==true){
+		nouvelleVariable.value.delete(newTime);
+	}
+	nouvelleVariable.value.set(newTime,newValue);
 	map.set(ancienneVariable.id,nouvelleVariable);
 }
-	
+
+//~ Copie la derniere valeur dans la nouvelle valeur temps 
+function copyOldValue(map,key,newTime){
+	var ancienneVariable = map.get(key);
+	var valeurCourante = ancienneVariable.lastValue;
+	var nouvelleVariable = new Variable(ancienneVariable.type,ancienneVariable.length,ancienneVariable.id,ancienneVariable.scope,ancienneVariable.name);
+	var ancienneValeur = ancienneVariable.value;
+	nouvelleVariable.value = ancienneValeur;
+	nouvelleVariable.value.set(newTime,valeurCourante);
+	map.set(ancienneVariable.id,nouvelleVariable);
+}
+
 //~ Parse le fichier vcd
 //~ listeVariables contient la liste des variables
 //~ etat represente l'etat des valeurs au cours du temps 
@@ -66,6 +98,8 @@ function parse(vcd){
 	var mots="";
 	//~ Tableau contenant la liste des variables du vcd
 	var listeVariables = new Map();
+	//~ Chaine représentant le module courant
+	var scope=""; 
 	
 	//~ -------  VARIABLE ------- ~//
 	affiche("---- DEBUT VARIABLE ----");
@@ -77,12 +111,22 @@ function parse(vcd){
 			b=false;
 			afficheMap(listeVariables);
 		}
+		else if(mots[0]=="$scope"){
+			scope=scope+mots[2]+".";
+		}
+		else if(mots[0]=="$upscope"){
+			scope = (scope.substring(0,scope.length-1));
+			scope= scope.substring(0,scope.lastIndexOf("."));
+			if(scope.length>0){
+				scope=scope+".";
+			}
+		}
 		else if(mots[0]=="$var"){
 			var type = mots[1];
 			var length = mots[2];
 			var id = mots[3];
 			var name = mots[4];
-			var nouvelleVariable = new Variable(type,length,id,name,"x");
+			var nouvelleVariable = new Variable(type,length,id,scope,name);
 			listeVariables.set(id,nouvelleVariable);
 		}
 		else{
@@ -92,27 +136,41 @@ function parse(vcd){
 	}
 	affiche("---- FIN VARIABLE ----");
 	
+	
 	//~ -------  VALEURS ------- ~//
 	affiche("---- DEBUT VALEURS ----");
 	var temps="";
 	var idVariable ="";
 	var valeurVariable ="";
+	var tableauTemps = new Array();
 	for(i;i<lignes.length;i++){
 		mots = lignes[i].split(' ');
 		if(mots[0].charAt(0)=='#'){
 			temps = mots[0].substring(1,mots[0].length);
+			tableauTemps.push(temps);
+			listeVariables.forEach(function(value,key){
+				copyOldValue(listeVariables,key,temps);
+			},listeVariables)
 			affiche("Nous sommes au temps : "+temps);
 		}
 		else if(mots[0].length==2){
 			valeurVariable=mots[0].charAt(0);
 			idVariable=mots[0].charAt(1);
+			changeMapValue(listeVariables,idVariable,temps,valeurVariable);
 			affiche("Id : "+idVariable +" valeur : "+valeurVariable);
 		}
 		else{
 			idVariable = mots[1];
 			valeurVariable = mots[0].substring(1,mots[0].length);
+			changeMapValue(listeVariables,idVariable,temps,valeurVariable);
 			affiche("Id : "+idVariable +" valeur : "+valeurVariable);
 		}
 	}
 	affiche("---- FIN VALEURS ----");
+
+	
+	listeVariables.forEach(function(value,key){
+		value.afficheTemps();
+		//~ affiche(value.afficheTempsA("0"));
+	},listeVariables)
 }
